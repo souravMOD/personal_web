55b69125999bb0c1c07f4d89ef112f269c21b600
sourav <souravv.modakk@gmail.com>
2026-02-02 23:09:11 +0100

    Configure Next.js static export and GitHub Pages deployment

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    domains: ['images.unsplash.com'],
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
